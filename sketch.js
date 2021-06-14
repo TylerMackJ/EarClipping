b0f14cbc0860f4472167c4b641b515d467a417b6
Tyler Johnson <Tylermackj@gmail.com>
2021-06-14 03:10:52 -0500

    Fixed major bugs - Infinite loop from not trying all triangles - Bad interesection test - - Swapped for internal point test

--- sketch.js
+++ sketch.js
@@ -107,25 +107,18 @@ function reset() {
 
 function earClip() {
   colorMode(HSB);
+  let fullPoly = [...points];
   while (points.length > 3) {
-    for (let i = 0; i < points.length - 3; i++) {
+    for (let i = 0; i < points.length; i++) {
+      let p = [i, (i + 1) % points.length, (i + 2) % points.length];
       // Testing triangle is i, i+1, i+2
       let goodTriangle = true;
 
-      // Test if line i->i+2 is not intersecting any other lines
-      for (let j = 0; j < points.length - 1; j++) {
-        if (i != j && i != j + 1 && i + 1 != j && i + 1 != j + 1 && i + 2 != j && i + 2 != j + 1) {
-          if(intersect(points[i], points[i + 2], points[j], points [j + 1])) {
-            goodTriangle = false;
-          }
-        }
-      }
-
       // Test if the center of of the triangle is inside the polygon
-      let center = [(points[i][0] + points[1 + 1][0] + points[i + 2][0]) / 3, (points[i][1] + points[1 + 1][1] + points[i + 2][1]) / 3]
+      let center = [(points[p[0]][0] + points[p[1]][0] + points[p[2]][0]) / 3, (points[p[0]][1] + points[p[1]][1] + points[p[2]][1]) / 3]
       let count = 0;
-      for (let j = 0; j < points.length - 1; j++) {
-        if (intersect(center, [Number.MAX_VALUE, center[1]], points[j], points[j + 1])) {
+      for (let j = 0; j < fullPoly.length - 1; j++) {
+        if (intersect(center, [Number.MAX_VALUE, center[1]], fullPoly[j], fullPoly[j + 1])) {
           count++;
         }
       }
@@ -133,11 +126,24 @@ function earClip() {
         goodTriangle = false;
       }
 
+      // Test if there are any points inside the triangle
+      for (let j = 0; j < points.length; j++) {
+        if (p[0] != j && p[1] != j && p[2] != j) {
+          if (pointInTriangle(points[j], points[p[0]], points[p[1]], points[p[2]])) {
+            goodTriangle = false;
+            print("Point inside");
+            break;
+          }
+        }
+      }
+
       if (goodTriangle) {
         fill(color(360 * Math.random(), 50, 90));
 
-        triangle(points[i][0], points[i][1], points[i + 1][0], points[i + 1][1], points[i + 2][0], points[i + 2][1]);
-        points.splice(i + 1, 1);
+        triangle(points[p[0]][0], points[p[0]][1], points[p[1]][0], points[p[1]][1], points[p[2]][0], points[p[2]][1]);
+        points.splice(p[1], 1);
+        print((p[1]) + " removed");
+        break;
       }
     }
   }
